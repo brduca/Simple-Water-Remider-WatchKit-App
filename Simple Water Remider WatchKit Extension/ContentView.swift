c4c081da650007a7d99c364423562ebbefd22b06
Salvador Mosca <salvador.mosca@ndrive.com>
2019-10-21 09:38:42 +0100

    first commit

--- Simple Water Remider WatchKit Extension/ContentView.swift
+++ Simple Water Remider WatchKit Extension/ContentView.swift
@@ -8,9 +8,80 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View
+{
+    private let amounts = ["100 ml", "200 ml", "300 ml", "400 ml"]
+    private let frequencies = ["2h", "3h", "4h", "5h"]
+    
+    @State private var selectedAmount = 0
+    @State private var selectedFrequency = 0
+    @State private var selectedStart = 0
+    @State private var selectedEnd = 0
+    
+    var notificationService: NotificationsServiceProtocol
+    
     var body: some View {
-        Text("Hello World")
+        
+        Form {
+            
+            Section {
+                
+                Picker(selection: $selectedAmount, label: Text("Amount"))
+                {
+                    ForEach(0 ..< amounts.count)
+                    {
+                        Text(self.amounts[$0])
+                    }
+                }
+            }
+            
+            Section {
+                
+                Picker(selection: $selectedFrequency, label: Text("Frequency"))
+                {
+                    ForEach(0 ..< frequencies.count)
+                    {
+                        Text(self.frequencies[$0])
+                    }
+                }
+            }
+            
+            Section {
+                
+                HStack  {
+                    Picker(selection: $selectedStart, label: Text("Start"))
+                    {
+                        ForEach(0 ..< frequencies.count)
+                        {
+                            Text(self.frequencies[$0])
+                        }
+                    }
+                    
+                    Picker(selection: $selectedEnd, label: Text("End"))
+                    {
+                        ForEach(0 ..< frequencies.count)
+                        {
+                            Text(self.frequencies[$0])
+                        }
+                    }
+                }
+            }
+            
+            Section {
+                
+                Button(action: {
+                    self.createNotifications()
+                    
+                }) {
+                    Text("[Remind me!]")
+                }
+            }
+        }
+    }
+        
+    private func createNotifications()
+    {
+        
     }
 }
 
---
+++ Simple Water Remider WatchKit Extension/NotificationsService.swift
@@ -0,0 +1,19 @@
+//
+//  NotificationsService.swift
+//  Simple Water Remider WatchKit Extension
+//
+//  Created by Admin on 21/10/2019.
+//  Copyright © 2019 homeland. All rights reserved.
+//
+
+import Foundation
+
+class NotificationsService: NotificationsServiceProtocol
+{
+   
+}
+
+protocol NotificationsServiceProtocol
+{
+    
+}
